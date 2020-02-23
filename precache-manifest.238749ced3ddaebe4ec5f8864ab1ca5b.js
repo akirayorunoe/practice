94a2de6658f12df09e8854de885f7c07c41bd13e
Thanh Hiền Phạm <akirayorunoe@gmail.com>
2020-02-23 20:44:26 +0700

    Updates

---
+++ precache-manifest.238749ced3ddaebe4ec5f8864ab1ca5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ab24ab68b7226484b7263bc26231edf",
+    "url": "/practice/index.html"
+  },
+  {
+    "revision": "7bdd332ec210b7bfea29",
+    "url": "/practice/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "38648462401f002c17b9",
+    "url": "/practice/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7bdd332ec210b7bfea29",
+    "url": "/practice/static/js/2.af7ae25c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/practice/static/js/2.af7ae25c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38648462401f002c17b9",
+    "url": "/practice/static/js/main.9de540f5.chunk.js"
+  },
+  {
+    "revision": "6ad52073a137c0c25cfa",
+    "url": "/practice/static/js/runtime-main.a7e18529.js"
+  }
+]);
